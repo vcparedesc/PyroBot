c5e1323e558689b8f01606a30eae47a082594a77
Victor Paredes <pvictorm6@gmail.com>
2017-09-26 02:05:25 -0500

    fixed indentation and added numpy and math support to evaluate dynamic matrices

--- pyro_expressions.py
+++ pyro_expressions.py
@@ -2,15 +2,20 @@ from sympy import *
 
 t = symbols('t')
 
-def print_python_expr(expr, file_handler, id, X):
+def print_python_expr(expr, file_handler, id, X, parameters):
     F = file_handler
     rows = expr.shape[0]
     cols = expr.shape[1]
-    F.write("def get_"+ id + "(x):\n\t")
+    F.write("def get_"+ id + "(self,x):\n\t\t")
     for idx in range(len(X)):
         F.write(str(X[idx]) + " = x[" + str(idx) + "]")
-        F.write('\n\t')
-    F.write('\n\t')
+        F.write('\n\t\t')
+        
+    for idx in range(len(parameters)):
+        F.write(str(parameters[idx]) + " = self." + str(parameters[idx]))
+        F.write('\n\t\t')
+                
+    F.write('\n\t\t')
     F.write("return np.matrix([")
     for row in range(rows):
         F.write("[")
@@ -21,19 +26,22 @@ def print_python_expr(expr, file_handler, id, X):
         F.write("]")
         if(row <= rows -2):
             F.write(", ")
-            F.write("\n\t")
+            F.write("\n\t\t")
     F.write("])")
     F.write('\n')
 
 def print_python_dynamics(fx,gx,X,parameters, values):
     F = open("PyroDynamics.py","w")
-    F.write("class PyroDynamics:\n")
-    F.write('\t')
+    F.write("import numpy as np \n")
+    F.write("from math import sin \n")
+    F.write("from math import cos \n\n")
+    F.write("class PyroDynamics:\n\t")
+    F.write("def __init__(self):\n\t\t")
     for idx in range(len(parameters)):
-        F.write(str(parameters[idx]) + " = " + str(values[idx]) + "\n\t")
+        F.write("self."+str(parameters[idx]) + " = " + str(values[idx]) + "\n\t\t")
     F.write("\n\t")
-    print_python_expr(fx,F,"fx",X)
+    print_python_expr(fx,F,"fx",X, parameters)
     F.write('\n')
     F.write('\t')
-    print_python_expr(gx,F,"gx",X)
+    print_python_expr(gx,F,"gx",X, parameters)
     F.close()
